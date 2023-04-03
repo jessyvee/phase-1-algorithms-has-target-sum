ee9a965a748a5c4ca85020841ef5af6a7b3090e7
Jesse <jessyvee1@gmail.com>
2023-04-03 20:36:21 +0300

    asignment done

--- index.js
+++ index.js
@@ -1,17 +1,41 @@
+
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for (let i = 0; i < array.length; i++){
+    const targetSum = target - array [i];
+
+   for (let k = i + 1; k < array.length; k++){
+    if (array [k] === targetSum) return true;
+
+  } 
+
+  }
+   return false; 
 }
 
 /* 
   Write the Big O time complexity of your function here
+  time complexity = O(n*2)
 */
 
 /* 
   Add your pseudocode here
+   for (let i = 0; i < array.length; i++){
+    const targetSum = target - array [i];
+   for (let k = i + 1; k < array.length; k++){
+    if (array [k] === targetSum) return true;
+  } 
+  }
+   return false; 
+}
 */
 
 /*
   Add written explanation of your solution here
+  We'll let our target minus the arrays provided and check whether the result can complement our array when added to provide the target
+  Our initializer will be targetSum (target - array[i])
+  We'll use the following approach to check whether the targetSum can be added to the array
+  If targetSum can be added to the array, then we'll return true
 */
 
 // You can run `node index.js` to view these console logs
@@ -29,6 +53,15 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([-7, 10, 4, 8], 3));
+
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([4], 4));
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([2, 2, 3, 3], 4));
 }
 
 module.exports = hasTargetSum;
